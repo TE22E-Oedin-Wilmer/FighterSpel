1ddfcd8569d269bea02d7f259ccfa80515b292cb
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2023-09-26 11:10:17 +0200

    DonesoKanske

--- FighterSpel/Program.cs
+++ FighterSpel/Program.cs
@@ -12,21 +12,30 @@
 
 // WHILE-LOOP
 
+using System.Collections.ObjectModel;
+using System.Drawing;
+
 int VaderHP = 100;
 int LukeHP = 100;
 
 while (VaderHP > 0 && LukeHP > 0)
 {
 
+
     Random generator = new Random();
 
     int damageVader = generator.Next(0, 15);
 
     int damageLuke = generator.Next(0, 15);
-
-    Console.WriteLine($"Vader HP: {VaderHP}");
-    Console.WriteLine($"Luke HP: {LukeHP}");
+Console.Clear();
+    Console.WriteLine($"Vader took {damageVader} damage!");
+    Console.WriteLine($"Luke took {damageLuke} damage!");
+    Console.WriteLine();
+    Console.WriteLine("Current HP:");
+    Console.WriteLine($"Vader: {VaderHP}");
+    Console.WriteLine($"Luke: {LukeHP}");
     Console.WriteLine("Keep dueling! Press [ENTER]");
+    Console.WriteLine();
     
     VaderHP -= damageVader;
     LukeHP -= damageLuke;
@@ -35,11 +44,16 @@ while (VaderHP > 0 && LukeHP > 0)
 
 if (LukeHP < VaderHP)
 {
+    Console.Clear();
+    Console.BackgroundColor = ConsoleColor.DarkRed;
     Console.WriteLine("Vader showed his true powers and won!");
+
 }
 
 else if (VaderHP < LukeHP)
 {
+    Console.Clear();
+    Console.BackgroundColor = ConsoleColor.Green;
     Console.WriteLine("Luke was to fast for his old man and won!");
 }
 
