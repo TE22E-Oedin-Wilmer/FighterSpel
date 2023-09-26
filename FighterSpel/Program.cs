c8112aa536464ceba9141175a7a19f95973a7791
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2023-09-26 10:48:11 +0200

    Doneso

--- FighterSpel/Program.cs
+++ FighterSpel/Program.cs
@@ -1,22 +1,52 @@
 ﻿// SLUMP
-//Random generator = new Random();
+// Random generator = new Random();
 
-//int slumptal = generator.Next(10, 20);
+// int slumptal = generator.Next(10, 20);
 
-//Console.WriteLine(slumptal);
+// int randomtal = generator.Next(10, 20);
+
+
+
+// Console.WriteLine(slumptal);
 
 
 // WHILE-LOOP
 
-// int tal = 100;
+int VaderHP = 100;
+int LukeHP = 100;
 
-// while (tal > 0) {
-    // Console.WriteLine(tal);
-    // tal --;
-// }
+while (VaderHP > 0 && LukeHP > 0)
+{
+
+    Random generator = new Random();
+
+    int damageVader = generator.Next(0, 15);
+
+    int damageLuke = generator.Next(0, 15);
+
+    Console.WriteLine($"Vader HP: {VaderHP}");
+    Console.WriteLine($"Luke HP: {LukeHP}");
+    Console.WriteLine("Keep dueling! Press [ENTER]");
+    
+    VaderHP -= damageVader;
+    LukeHP -= damageLuke;
+    Console.ReadKey();
+}
+
+if (LukeHP < VaderHP)
+{
+    Console.WriteLine("Vader showed his true powers and won!");
+}
+
+else if (VaderHP < LukeHP)
+{
+    Console.WriteLine("Luke was to fast for his old man and won!");
+}
+
+
 
 
 // Console.WriteLine("FÄRDIG");
 
 
-// Console.ReadLine();
+Console.ReadLine();
